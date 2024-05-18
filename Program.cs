147414e153b34b80cf9fa635aff25a9a5cd713b0
mikepreston17 <michael.n.preston@gmail.com>
2024-05-18 06:30:59 -0500

    now slq works (but rider/sudo's being a whiny b*tch about accessing obj/*tmp files.  Thanks whoever messed that up.  Java coders, amirite?

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using CodeMechanic.Systemd.Daemons;
 using CodeMechanic.Types;
+using CodeMechanic.FileSystem;
+using CodeMechanic.Diagnostics;
 using Coravel;
 using Coravel.Invocable;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,9 +13,9 @@ public class MyFirstInvocable : IInvocable
 {
     public async Task Invoke()
     {
-        Console.WriteLine("Hello from personal_daemon! (updated at 6am)");
+        Console.WriteLine("Hello from personal_daemon! (updated at 7am)");
         /// Sample MySQL logging (requires MYSQL_* .env variables to be set in your new .env).
-        if (Environment.GetEnvironmentVariable("MYSQLPASSWORD").NotEmpty())
+        if (Environment.GetEnvironmentVariable("MYSQLPASSWORD").Dump("here's the f*cking password").NotEmpty())
         {
             int rows = await MySQLExceptionLogger.LogInfo($"Invoking from /srv/{nameof(personal_daemon)}!",
                 nameof(personal_daemon));
@@ -26,6 +28,7 @@ public class Program
 {
     public static async Task Main(string[] args)
     {
+        DotEnv.Load();
         IHost host = CreateHostBuilder(args)
             .UseSystemd()
             .Build();
